4c89c636d742d9d4ba58f787c73268fb57372b30
Ilhammansiz188 <80038885+Ilhammansiz188@users.noreply.github.com>
2021-03-05 18:08:32 +0700

    Update StringSession.js

--- whatsasena/StringSession.js
+++ whatsasena/StringSession.js
@@ -1,9 +1,9 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/* Copyright (C) 2020 ILham UserBot wa.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 
-WhatsAsena - Yusuf Usta
+Ilham mansiz - ILham UserBot wa
 */
 
 const fs = require('fs');
@@ -13,7 +13,7 @@ class StringSession {
     }
 
     deCrypt(string = undefined) {
-        if ('ASENA_SESSION' in process.env && string === undefined) {
+        if ('ilhamuserbot_SESSION' in process.env && string === undefined) {
             string = process.env.STRING_SESSION;
         } else if (string !== undefined) {
             if (fs.existsSync(string)) {
@@ -28,7 +28,7 @@ class StringSession {
     }
 
     createStringSession(dict) {
-        return 'ASENA;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
+        return 'ilhamuserbotwa;;;' + Buffer.from(JSON.stringify(dict)).toString('base64');
     }
 }
 
